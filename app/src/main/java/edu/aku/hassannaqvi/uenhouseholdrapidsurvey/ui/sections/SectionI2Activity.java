7ab9f79cb18df340e6bf63291c6cbf8cc3923b1e
shznaqvi <shznaqvi@gmail.com>
2021-12-06 20:54:07 +0500

    Activities Generated

---
+++ app/src/main/java/edu/aku/hassannaqvi/uenhouseholdrapidsurvey/ui/sections/SectionI2Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uenhouseholdrapidsurvey.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.uenhouseholdrapidsurvey.R;
+
+public class SectionI2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_i2);
+    }
+}
